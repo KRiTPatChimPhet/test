ba5c399cc23c8f9150955a05c2d4a1822d671f65
Krit Patchimphet <kritpatchimphet4@gmail.com>
2024-10-29 17:35:55 +0700

    fix

--- src/app/core/socket/socket.service.ts
+++ src/app/core/socket/socket.service.ts
@@ -9,7 +9,7 @@ export class SocketService {
     private socketOrder: Socket;
 
     constructor() {
-        this.socketOrder = io('http://localhost:3000/order', {
+        this.socketOrder = io('http://localhost:3000/orders', {
             transports: ['websocket'],
             auth: {
                 token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDhhODQyYTJhNGJhZTAwMTkzNmNiYTkiLCJmaXJzdE5hbWUiOiLguIHguKTguJUiLCJsYXN0TmFtZSI6IuC4m-C4seC4iOC4ieC4tOC4oeC5gOC4nuC5h-C4iuC4oyIsImRlcGFydG1lbnQiOiJBTEwiLCJyb2xlcyI6WyJhZG1pbiJdLCJpYXQiOjE3MzAxMDY0MDQsImV4cCI6MTczMDE5MjgwNCwiYXVkIjoiTGFtdW5waGFuIiwiaXNzIjoiTGFtdW5waGFuIn0.Uo-Re5fFAoXGHMatyPDsKt7G5BihqEli-5dSQKkBuYs',
@@ -18,12 +18,14 @@ export class SocketService {
     }
 
     sendMessage(message: string) {
-        this.socketOrder.emit('order', message);
+        this.socketOrder.emit('message', message);
     }
 
     getMessage(): Observable<string> {
         return new Observable((observer) => {
-            this.socketOrder.on('order', (data: string) => observer.next(data));
+            this.socketOrder.on('message', (data: string) =>
+                observer.next(data)
+            );
         });
     }
 
